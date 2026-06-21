6821cb04a13c02fea06f9009d70dd7b11910da9c
tainaperesribeiro83-star <tainaperesribeiro83@gmail.com>
2026-06-20 22:13:41 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Agenda Colorida',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ task.dart
@@ -0,0 +1,10 @@
+
+class Task {
+  String title;
+  bool completed;
+
+  Task({
+    required this.title,
+    this.completed = false,
+  });
+}
